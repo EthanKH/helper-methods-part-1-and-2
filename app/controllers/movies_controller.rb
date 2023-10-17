dec52f3103a1a475d2c7e4fd336cfddfdc4589dc
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-17 16:35:44 +0000

    lots of refactoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,16 @@
 class MoviesController < ApplicationController
-  def new
-    @the_movie = Movie.new
-  end
+ 
 
   def index
-    matching_movies = Movie.all
+    # matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    # @movies = matching_movies.order({ :created_at => :desc })
+    @movies = Movie.order(created_at: :desc)
 
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
       format.html do
@@ -20,24 +19,35 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch(:id)
 
-    # matching_movies = Movie.where({ :id => the_id })
-    matching_movies = Movie.where(id: the_id)
+    the_id = params.fetch(:id) # First get iD
 
+    # matching_movies = Movie.where({ :id => the_id }) 
+    matching_movies = Movie.where(id: the_id) # Second get matching rows (ActiveRecord:Relation)
 
-    @the_movie = matching_movies.first
+    @movie = matching_movies.first # Third get the one instance of ActiveRecord/one row
+
+    # @the_movie = Movie.where(id: params.fetch(:id)).first # this single line tends to be used
+    # @the_movie = Movie.find_by(:id params.fetch(:id)) # this returns a nil if there's no id, or 500 error page
+    # @the_movie = Movie.find(params.fetch(:id)) # throws 404 error exception if ID doesn't exist
+    # @movie = Movie.find(params.fetch(:id))
+
+  end
+
+  def new
+    @movie = Movie.new
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @movie = Movie.new
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
 
-    if @the_movie.valid?
-      @the_movie.save
+    if @movie.valid?
+      @movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
+      redirect_to movies_url, notice: "error"
     end
   end
 
@@ -47,33 +57,33 @@ class MoviesController < ApplicationController
     # matching_movies = Movie.where({:id => the_id })
     matching_movies = Movie.where(id: the_id)
 
-    @the_movie = matching_movies.first
+    @movie = matching_movies.first
 
   end
 
   def update
     the_id = params.fetch(:id)
     # the_movie = Movie.where({ :id => the_id }).first
-    the_movie = Movie.where(id: the_id).first
+    movie = Movie.where(id: the_id).first
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    movie.title = params.fetch("query_title")
+    movie.description = params.fetch("query_description")
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to "/movies/#{the_movie.id}", notice: "Movie updated successfully."
+    if movie.valid?
+      movie.save
+      redirect_to "/movies/#{movie.id}", notice: "Movie updated successfully."
     else
-      redirect_to "/movies/#{the_movie.id}", alert: "Movie failed to update successfully."
+      redirect_to "/movies/#{movie.id}", alert: "Movie failed to update successfully."
     end
   end
 
   def destroy
     the_id = params.fetch(:id)
     # the_movie = Movie.where({ :id => the_id }).first
-    the_movie = Movie.where(id: the_id).first
+    movie = Movie.where(id: the_id).first
     
 
-    the_movie.destroy
+    movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
